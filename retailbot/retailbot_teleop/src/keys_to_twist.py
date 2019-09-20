23a294455cf0aaae34571537a8ef4eae483d19a2
mohamed <mohamedaminhouidi@gmail.com>
2019-09-20 10:43:31 +0100

    student workspace

--- retailbot/retailbot_teleop/src/keys_to_twist.py
+++ retailbot/retailbot_teleop/src/keys_to_twist.py
@@ -14,17 +14,8 @@ def keys_cb(msg, twist_pub):
         target velocities are extracted from the dictionary and published
         to the twist_pub topic.
         '''
-	if len(msg.data) == 0 or msg.data[0] not in key_mapping:
-		return # unknown key
-	vels = key_mapping[msg.data[0]]
-	t = Twist()
-	t.angular.z = vels[0]
-	t.linear.x = vels[1]
-	twist_pub.publish(t)
+	
 
 
 if __name__ == '__main__':
-	rospy.init_node('keys_to_twist')
-	twist_pub = rospy.Publisher('key_vel', Twist, queue_size=1)
-	rospy.Subscriber('keys', String, keys_cb, twist_pub)
-	rospy.spin()
+	
--- retailbot/retailbot_teleop/src/keys_to_twist_using_rate.py
+++ retailbot/retailbot_teleop/src/keys_to_twist_using_rate.py
@@ -5,7 +5,7 @@
     by simply repeating the last motion command if a new key was not pressed.
 '''
 
-# BEGIN ALL
+
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
@@ -13,26 +13,10 @@ from geometry_msgs.msg import Twist
 key_mapping = { 'z': [ 0, 1], 'x': [0, -1], 
                 'q': [-1, 0], 'd': [1,  0], 
                 's': [ 0, 0] }
-g_last_twist = None 
+ 
 
 def keys_cb(msg, twist_pub):
-  global g_last_twist
-  if len(msg.data) == 0 or msg.data[0] not in key_mapping:
-    return # unknown key.
-  vels = key_mapping[msg.data[0]]
-  g_last_twist.angular.z = vels[0]
-  g_last_twist.linear.x  = vels[1]
-  twist_pub.publish(g_last_twist)
+  
 
 if __name__ == '__main__':
-  rospy.init_node('keys_to_twist')
-  twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-  rospy.Subscriber('keys', String, keys_cb, twist_pub)
-  # BEGIN RATE
-  rate = rospy.Rate(10) #set rate to 10 Hz
-  g_last_twist = Twist() # initializes to zero
-  while not rospy.is_shutdown():
-    twist_pub.publish(g_last_twist)
-    rate.sleep() #this will ensure the loop runs exactly at 10 Hz 
-  # END RATE
-# END ALL
+  
